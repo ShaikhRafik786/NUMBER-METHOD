c444d19057262953e273de9559a872ecc706f326
Shaikh RAFIK <107038358+ShaikhRafik786@users.noreply.github.com>
2023-02-04 23:41:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+// ////------methods of number in javascript------///////
+
+// 1.Number---> it convert  string  into number  ///
+// var num="143";
+// var a = Number(num);
+// console.log(typeof(a));
+
+// let rk = "358221323";
+// let srk = Number(rk);
+// console.log(srk)
+
+// 2.TO string--->it converts number  into string it always returns a string/////it always returns a string//
+// var num = 7865884858;
+// var s = num.toString();
+// console.log(typeof(s));
+
+
+// var num = 786786;
+// var sk = toString(num);
+// console.log(sk);
+
+//3. parseInt: parses a string and returns a whole number. Spaces are allowed. Only the first number is returned
+// to parseInt in whole no deta koi bhi fractional no ka
+// var x=93.81;
+// var a=parseInt(x);
+// console.log(a);
+
+// let rk =999.54;
+// let srk = parseInt(rk);
+// console.log(srk);
+
+
+// 4. parseFloat: parses a string and returns a number. Spaces are allowed. Only the first number is returned
+//to parseFloat
+// var y="17.870005";
+// var  r = parseFloat(y)
+// console.log(r);
+
+
+
+//5. toPrecision ---> return a string with a number return with a specified length
+// var x=9.4943434;
+// var s=x.toPrecision(5);
+// console.log(s);
+
+
+
+//6. to Fixed: returns a string, with the number written with a specified number of decimals://
+// to Fixed return astring with a number return with a specified no of decimals
+// var x=9.49;
+// var x=x.toFixed();
+// console.log(x);
+
+
+
+
+// 2. to Exponential: returns a string, with a number rounded and written using exponential notation.
+//to Expontial a string with a rounded & return using exponential notation
+// var x=98.494;
+//  var x=x.toExponential(3);
+//  console.log(x);
